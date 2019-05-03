1b553c948536a6691ed206b89b9bd58467e40caf
Harvey Breaux <hbreauxv@gmail.com>
2019-05-03 12:18:26 -0600

    Initial project version

---
+++ README.md
@@ -0,0 +1,9 @@
+# NCM-API-Class
+Working with the NCM API, made easy.  This repository is my attempt to use what i've been learning about OOP and make some classes for the NCM API that "just work".  They handle the basics so that you don't have to. 
+
+NCM_API_Object.py is two classes intended to do a lot of useful stuff with the NCM API
+
+NCMRouterConfig is for getting, putting, and patching configuration to routers in NCM
+NCMGroupConfig is for getting, putting, and patching configuration to groups.
+
+This script is a work in progress.  Made by Harvey Breaux for use with Cradlepoint.  
---
+++ test.py
@@ -0,0 +1,29 @@
+from NCM_API import NCMRouterConfig
+
+# fill these in with your NCM headers
+headers = {
+    'X-CP-API-ID': 'xxxx',
+    'X-CP-API-KEY': 'xxxx',
+    'X-ECM-API-ID': 'xxxx',
+    'X-ECM-API-KEY': 'xxxx',
+    'Content-Type': 'application/json'
+}
+
+# replace this payload with whatever you want.  If you put or patch this one it should give you a 409 response.
+payload = \
+    {
+        "configuration": [{
+            "example": "this wont work because it ncm wont like it"
+
+        },
+            []
+        ]
+    }
+
+
+# this is a test get. replace the router IDs in here with some of the router IDs in your NCM account.
+NCMTesting = NCMRouterConfig(headers)
+result = NCMTesting.get([1111976,695606])
+
+print(result[1111976])
+print(result[695606].content)
